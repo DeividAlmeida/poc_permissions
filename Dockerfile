01b8ea99d389288663c3c0ef13e9013efce99c37
Deivid Almeida <mr.davialmeida@hotmail.com>
2024-09-04 17:46:02 -0300

    feat: add port env

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,9 @@
-# Etapa de build
 FROM ubuntu:24.04 
 
-# Defina o diretório de trabalho dentro do contêiner
 WORKDIR /usr/src/app
 
-# Atualize os pacotes e instale o Redis
 RUN apt-get update && apt-get install -y redis-server
 
-# Copie o arquivo Cargo.toml e Cargo.lock
-COPY Cargo.toml Cargo.lock ./
-
-# Copie o código fonte da aplicação
 COPY . .
 
-# Comando para iniciar o Redis e a aplicação
 CMD ["sh", "-c", "redis-server & ./permissions"]
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum;
+use dotenv::var;
 mod controllers;
 mod serializers;
 mod routes;
@@ -7,7 +8,9 @@ mod middleware;
 
 #[tokio::main]
 async fn main() {
+    let port = var("PORT").expect("DB_URL must be set");
+    let url = format!("0.0.0.0:{}", port);
     let routes = routes::routes();
-    let listener: tokio::net::TcpListener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let listener: tokio::net::TcpListener = tokio::net::TcpListener::bind(url).await.unwrap();
     axum::serve(listener, routes).await.unwrap();
 }
